d77743a37d452dcc0a5fb3769e7b347ff2aecf3e
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2024-01-02 15:16:39 -0800

    loops

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-        if number < 0:
-            number = -number
-        LAST = number % 10
-        print(LAST, end="")
-        return LAST
+    if number < 0:
+        number = -number
+    LAST = number % 10
+    print(LAST, end="")
+    return LAST
