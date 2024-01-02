8777fd91068c87e70dcd69f00a5d7bb042c6159e
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2024-01-02 13:41:26 -0800

    loops

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-if chr(i) != 'e' and chr(i) != 'q':
-print('{:c}'.format(i), end='')
+for a in range(97, 123, 1):
+    if ((a == 101) or (a == 113)):
+        continue
+    print("{}".format(chr(a)), end='')
