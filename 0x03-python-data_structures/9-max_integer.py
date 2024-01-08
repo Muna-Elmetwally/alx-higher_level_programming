914e75672dfd5ec9f8a910f55f20a4795da8646d
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2024-01-08 12:32:56 -0800

    data structure

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
+    if 0 == len(my_list):
         return None
-    else:
-        my_list.sort()
-        return((my_list[-1]))
+    return sorted(my_list)[-1]
