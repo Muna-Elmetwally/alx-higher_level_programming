55db930853b9e57e9aa0b87c68d95e7f2172eb06
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2024-02-06 14:30:04 -0800

    python

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
-""" Module that contains a function that reads from a file """
 
 
 def read_file(filename=""):
-    """ Function that reads from a file
-
-    Args:
-        filename: filename
-
-    Raises
-        Exception: when the file can be opened
-
-    """
-
-    with open(filename, 'r', encoding="utf-8") as f:
-        read_data = f.read()
-        print(read_data, end='')
-
+    with open(filename, "r", encoding="UTF-8") as f:
+        print(f.read(), end="")
